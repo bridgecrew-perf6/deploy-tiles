d617290bcad896dd3223fc9486fec695f8a72dc9
Danil Kudabaev <danil@unit.spb>
2022-02-09 17:24:17 +0300

    By works, I meant 'doesnt work'.  Works now..

--- deploy-tiles.sh
+++ deploy-tiles.sh
@@ -1,38 +1,48 @@
 #!/bin/bash
 
 set -e
-ERR_NEED_CURL=13
+
 WEEKLY_FOLDER=ftp://ftp.spb.vega.su/pub/weekly/
-INDEX_FILE=index.tiles
 MPROXY_CONF=/srv/geodata/cache/mproxy_conf.shelve.yaml
 
-env curl --version &>/dev/null || {
-    echo "Ошибка: для работы необходима утилита curl. Команда для установки: sudo apt install -y curl"
-    exit ${ERR_NEED_CURL}
-}
+if ! env curl --version &>/dev/null 
+then
+    echo "Для загрузки обновлений необходима утилита curl. Команда для установки: sudo apt install -y curl"
+else
+    echo "Получение списка файлов с сервера обновлений ${WEEKLY_FOLDER}" 
+    REMOTE_FILE_LIST=$(curl --silent --list-only ${WEEKLY_FOLDER}) || echo "Нет подключения к серверу обновлений"
 
-echo "Получение списка файлов с сервера обновлений ${WEEKLY_FOLDER}" 
-REMOTE_FILE_LIST=$(curl --silent --list-only ${WEEKLY_FOLDER}) || echo "Нет подключения к серверу обновлений"
+    echo -e "Доступны для загрузки файлы \n ${REMOTE_FILE_LIST}"
+    read -i "н" -p "Загрузить файлы (д/н)? " download_enabled
 
-echo -e "Доступны для загрузки файлы \n ${REMOTE_FILE_LIST}"
-read -i "н" -p "Загрузить файлы (д/н)?" download_enabled
+    case "${download_enabled}" in
+    "д")
+        echo "Начинается загрузка файлов"
+        for tar in ${REMOTE_FILE_LIST}
+        do
+            if [[ "${tar}" =~ .*tar ]]
+            then
+                echo "Загружается ${WEEKLY_FOLDER}/${tar}"
+                curl --remote-name --continue-at - ${WEEKLY_FOLDER}/${tar}
+            fi
+        done
+        ;;
+    "н")
+        echo "Отказ от загрузки."
+        ;;
+    *)
+        echo "Ни да, ни нет. Продолжаем"
+        ;;
+    esac
+fi
 
-case "${download_enabled}" in
-"д")
-    echo "Начинается загрузка файлов"
-    for tar in ${REMOTE_FILE_LIST}
-    do
-        if [[ "${tar}" =~ .*tar ]]
-        then
-            echo "Загружается ${WEEKLY_FOLDER}/${tar}"
-            curl --remote-name --continue-at - ${WEEKLY_FOLDER}/${tar}
-        fi
-    done
-    ;;
-"н")
-    echo "Отказ от загрузки."
-    ;;
-*)
-    echo "Ни да, ни нет. Продолжаем"
-    ;;
-esac
+echo "Поиск конфигурационного файла mapproxy"
+if [ -e ${MPROXY_CONF} ]
+then
+    echo "Найден ${MPROXY_CONF}"
+    
+    ACTUAL_FILE_LIST=*.tar
+    echo "Распаковать архивы?"
+    echo ${ACTUAL_FILE_LIST}
+    
+fi
